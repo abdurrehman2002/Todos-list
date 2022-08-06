e9bf6afc6c2bd4f916bd4d85e06fcc1a1572840f
Rehman <abdurrehman.ptf786@gmail.com>
2022-08-06 15:54:39 +0500

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam enim vero,
+                nulla sed obcaecati possim  us, ad pariatur quis dignissimos  totam,
+                incidunt minus modi inventore nisi perspiciatis saepe deleniti quod
+                recusandae assumenda facilis aperiam numquam?</p>
+        </div>
+    );
+}
+
